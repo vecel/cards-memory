ccb739913a8c2f7f6fa11b9994c88ef109ade4aa
Mateusz Karandys <mateusz.karandys@gmail.com>
2021-07-15 15:47:41 +0200

    show/hide settings

--- memory-game-script.js
+++ memory-game-script.js
@@ -1,5 +1,15 @@
-const settings = document.querySelector('li.settings');
+const settings = document.querySelector('li #settings');
+const settingsList = document.querySelector('ul.settings-options');
+
+console.log(settings)
 
 settings.addEventListener('click', () => {
     console.log('123');
+    const currentDisplay = getComputedStyle(settingsList).getPropertyValue('display');
+    if (currentDisplay === 'none') {
+        settingsList.style.display = 'flex';
+    }
+    if (currentDisplay === 'flex') {
+        settingsList.style.display = 'none';
+    }
 })
