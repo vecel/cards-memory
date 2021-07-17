eebc1138007b6d272cedbfb5c2fdbcc41f11bcf0
Mateusz Karandys <mateusz.karandys@gmail.com>
2021-07-17 13:54:42 +0200

    game buttons

--- memory-game-script.js
+++ memory-game-script.js
@@ -1,7 +1,9 @@
 const settings = document.querySelector('li #settings');
 const settingsList = document.querySelector('ul.settings-options');
 
-console.log(settings)
+const startButton = document.querySelector('button.start-game');
+const stopButton = document.querySelector('button.stop-game');
+const nextButton = document.querySelector('button.next');
 
 settings.addEventListener('click', () => {
     console.log('123');
@@ -13,3 +15,38 @@ settings.addEventListener('click', () => {
         settingsList.style.display = 'none';
     }
 })
+
+startButton.addEventListener('click', () => {
+    console.log('start');
+    startGame();
+})
+
+stopButton.addEventListener('click', () => {
+    console.log('stop');
+    // stop playing game
+})
+
+nextButton.addEventListener('click', () => {
+    console.log('next');
+})
+
+
+
+
+function startGame() {
+    setUpButtons();
+}
+
+function setUpButtons() {
+    setDisplayNone(startButton);
+    setDisplayBlock(stopButton);
+    setDisplayBlock(nextButton);
+}
+
+function setDisplayNone(element) {
+    element.style.display = 'none';
+}
+
+function setDisplayBlock(element) {
+    element.style.display = 'block';
+}
